a4168871536f93ca276ec11758656fddec194d7f
mars <578595193@qq.com>
2017-09-07 09:28:04 +0800

    custom token

--- app/controllers/concerns/authentication_rails/application_helper.rb
+++ app/controllers/concerns/authentication_rails/application_helper.rb
@@ -5,12 +5,25 @@ module AuthenticationRails::ApplicationHelper
     end
   end
 
+  protected
+  def sign_authenticate_token token
+    "#{token}#{md5_sign_string(token)}"
+  end
+
+  private
   def authenticate_user
     token, option = ActionController::HttpAuthentication::Token.token_and_options(request)
     head :unauthorized unless valid_authenticate_token?(token)
   end
 
   def valid_authenticate_token? token
-    @current_user = token && User.find_by(authentication_token: token)
+    if (split_token = token&.split('.')) && split_token.length == 2 && sign_authenticate_token(split_token[0]) == token
+      @current_user = split_token[0] && User.find_by(authentication_token: split_token[0])
+    end
+  end
+
+  def md5_sign_string string, secret_key = nil
+    string = "#{string}#{secret_key || Rails.application.secrets.secret_key_base}"
+    Digest::MD5.hexdigest(string)
   end
 end
